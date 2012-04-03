b2b56bf29c1a986f184db496e1a115abc5530b37
Sarat T <saratgr8@gmail.com>
2012-04-02 22:12:09 -0700

    working rspec test created for MobileworksApi

--- lib/mobileworks_api.rb
+++ lib/mobileworks_api.rb
@@ -1,10 +1,10 @@
 # the mobile works api should go here. 
 require 'json'
 
-class MobileWorks
+class MobileworksApi
   #TODO: DRY out code by giving get_response() more responsibilities
 
-  def post_task(instructions, fields, options={})
+  def self.post_task(instructions, fields, options={})
     
     #converts ALL params to JSON. instructions and fields are mandatory, and therefore not part of a hash
     params = {:instructions=>instructions, :fields=>fields}
@@ -19,14 +19,14 @@ class MobileWorks
     hash_response = JSON.parse(response)
     @task_uri = hash_response["Location"]
 
+    #post_task returns http, but retrieve_task requires https. TODO: clean up this smelly LOC
+    @task_uri = @task_uri.gsub("http:","https:")
+
     return @task_uri
 
   end
 
-  def retrieve_task(task_uri)
-
-    #post_task returns http, but retrieve_task requires https. TODO: clean up this smelly LOC
-    task_uri = task_uri.gsub("http","https")
+  def self.retrieve_task(task_uri)
 
     response = get_response("curl " + task_uri + " -u crowd_ass:cs169")
 
@@ -36,7 +36,7 @@ class MobileWorks
   end
 
   private
-  def get_response(request)
+  def self.get_response(request)
 
     response = %x(#{request})
     return response
@@ -47,7 +47,7 @@ end
 
 #SAMPLE USE CASE
 #-----------------------------------------------------------------------------------
-#m = MobileWorks.new
+#m = MobileworksApi.new
 #task_uri = m.post_task("How to write rpsec tests for files in lib dir",[{:name=>"t"}], {:resource=>"http://www.mobileworks.com/developers/images/samplecard.jpg"})
 #
 #task_status = m.retrieve_task(task_uri)
--- spec/lib/mobileworks_api_spec.rb
+++ spec/lib/mobileworks_api_spec.rb
@@ -1,21 +1,41 @@
 require 'spec_helper'
+require 'mobileworks_api'
 #require File.expand_path(File.dirname(__FILE__) + '/../spec_helper') 
 
 describe MobileworksApi do
-  
+
   describe "#post_task" do
     it "should post the task and return task uri" do
-      
-      @api = MobileWorks.new
-      MobileWorks.should_receive(:get_response).with(anything()).and_return("http://work.mobileworks.com/api/v2/task/168308/")
 
-      @task_uri = @api.post_task("instructions lol", "fields lol")
-      
+      #@api = MobileworksApi.new
+      @@taskUri = "http://work.mobileworks.com/api/v2/task/168326/" 
+      taskUriJson = '{"Location":"' + @@taskUri + '"}'
+      MobileworksApi.stub(:get_response).and_return(taskUriJson)
+
+      @task_uri = MobileworksApi.post_task("instructions lol", "fields lol")
+      @task_uri.should == @@taskUri.gsub("http:","https:") 
 
     end
-
   end
+  describe "#retrieve_task" do
+    it "should retrieve the task given by task uri" do
+      @@taskUri = @@taskUri.gsub("http:","https:") 
+      MobileworksApi.should_receive(:retrieve_task).with(@@taskUri).and_return(hash_including(:answer=>anything(),
+                                                                                              :confidence=>anything(),
+                                                                                              :status=>anything(),
+                                                                                              :instructions=>anything(),
+                                                                                              :redundancy=>anything(),
+                                                                                              :workflow=>anything(),
+                                                                                              :resource=>anything(),
+                                                                                              :resourcetype=>anything(),
+                                                                                              :resource=>anything(),
+                                                                                              :resourcetype=>anything(),
+                                                                                              :resource_url=>anything(),
+                                                                                              :timeCreated=>anything(),
+                                                                                              :timeFinished=>anything()))
+      response_hash = MobileworksApi.retrieve_task(@@taskUri)
 
-
+    end
+  end
 
 end
