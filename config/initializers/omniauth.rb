e8feab546579bc3480853fc47c67d1830ae106e5
Tanooj Luthra <tanoojl@gmail.com>
2012-04-18 21:00:16 -0700

    Fixed omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :twitter, 'OsApcB2lpW9CmqXjDaFqw', 'ivi3McKXwMHsN6mXiTCZQtbYUruXuEYbOu2oMqWZjM'
-  if Rails.env.production? provider :facebook, '407994262545802', '2b10ac73633f1b4212217ad74b9dff05'
+  if Rails.env.production? 
+  	provider :facebook, '407994262545802', '2b10ac73633f1b4212217ad74b9dff05'
   else
 	provider :facebook, '325736630808754', '8119d3396ebd3dfbf501ee6afdde4621'
   end
