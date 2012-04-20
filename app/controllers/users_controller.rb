b82889ae7aa1604c661632a32092d3ba8554b6a3
Tanooj Luthra <tanoojl@gmail.com>
2012-04-20 01:38:43 -0700

    @users to @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @users }
+      format.json { render json: @user }
     end
   end
 
